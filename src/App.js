3acd541027f41d51fd689fb6612aeb922168cfbf
Gustavo <gustavomfernandes6@gmail.com>
2021-08-16 07:57:36 -0300

    Primeiro

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+function Page1() {
+  return <h1>Esta é a pagina 1</h1>
+}
+
+function Page2() {
+  return <h1>Esta é a pagina 2</h1>
+}
+
+  function App() {
+
+    let currentPage = 2;
+
+    return (
+    <div className="App">
+      {currentPage === 2 ? <Page1 /> : <Page2 />}
+    </div>
+  );
+}
+
+export default App;
