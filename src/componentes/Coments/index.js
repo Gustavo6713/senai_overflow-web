fdb2b60853eb159cfd0d2962f6fae39a3e0b59f9
Gustavo <gustavomfernandes6@gmail.com>
2021-08-18 11:23:00 -0300

    atualizado 18/08/2021

---
+++ src/componentes/Coments/index.js
@@ -0,0 +1,20 @@
+import { Container } from "./styles";
+
+function Coments() {
+    return (
+        <Container>
+            <header>
+                <img src="https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_user2-128.png" />
+                <strong>
+                    Amanda Silva 
+                </strong>
+                <p>
+                    em 18/08/2021 às 10:32
+                </p>
+            </header>
+            <p>Viajando com que dinheiro? Desempregado!</p>
+        </Container>
+    );
+}
+
+export default Coments;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,64 @@
 import Header from "../../componentes/Header";
+import Coments from "../../componentes/Coments";
+
+import { FeedContainer, GistIcon, Main, Post } from "./styles";
 
 function Home(){
 
     return (
         <>
             <Header />
-            <main>
+            <Main>
                 <nav>
-
+                    Profile
                 </nav>
-                <section>
-
-                </section>
+                <FeedContainer>
+                   <PostCard/>
+                   <PostCard/>
+                   <PostCard/>
+                   <PostCard/>
+                </FeedContainer>
                 <aside>
-
+                    Actions
                 </aside>
-            </main>
+            </Main>
         </>
     );
+}
+
+function PostCard() {
+    return (
+        <Post>
+            <header>
+                <img src="https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_user2-128.png"/>
+                <div>
+                    <strong>
+                        Gustavo Marques Fernandes
+                    </strong>
+                    <p>
+                        em 18/08/2021 às 08:50
+                    </p>
+                </div>
+                <GistIcon/>
+            </header>
+            <main>
+                <div>
+                    <h1>Título</h1>
+                    <p>Descrição da postagem...</p>
+                </div>
+                <img src="https://images.memphistours.com/xlarge/54dad79c1e9a650dd95acea2cae6940d.jpg"/>
+                <section>
+                    <p>Front-end</p>
+                    <p>CSS</p>
+                    <p>JS</p>
+                </section>
+            </main>
+            <footer>
+                <h2>Comentários</h2>
+                <Coments/>
+            </footer>
+    </Post>
+    )
 }
 
 export default Home;
